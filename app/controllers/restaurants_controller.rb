de33e1cec8538d192269afc912b3d42c1598b19b
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-10-18 22:47:50 +0100

    Reviews new route+view added w/ form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[show] # add for each action (creatE)
+  before_action :set_restaurant, only: %i[show] # add for each action (create)
 
   def index
     @restaurants = Restaurant.all
@@ -11,10 +11,19 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path(@restaurant)
+  end
 end
 
 private
 
 def set_restaurant
   @restaurant = Restaurant.find(params[:id])
+end
+
+def restaurant_params
+  params.require(:restaurant).permit(:name, :category, :address, :phone_number)
 end
