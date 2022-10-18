34d303573be25654e946531f0cb9bbcee6d29aa5
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-10-18 19:59:36 +0100

    Restaurants db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
 require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+13.times do
+  restaurant = Restaurant.create(
+    name: ["The #{Faker::Verb.ing_form.capitalize} #{Faker::Food.ingredient}", Faker::Restaurant.name].sample,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "\"#{restaurant.name}\" added"
+end
+
+puts "#{Restaurant.all.size} restaurants added to db"
