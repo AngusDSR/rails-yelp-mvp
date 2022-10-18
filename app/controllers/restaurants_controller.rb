abcfc0d80d1d280a3d736ec3e58c98d23bb85298
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-10-18 20:49:38 +0100

    Restaurants show view added & 'styled'

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,15 @@
 class RestaurantsController < ApplicationController
-  # before_action @restaurant = Restaurant.find(params[:id])
+  before_action :set_restaurant, only: %i[show] # add for each action
 
   def index
     @restaurants = Restaurant.all
   end
+
+  def show; end
+end
+
+private
+
+def set_restaurant
+  @restaurant = Restaurant.find(params[:id])
 end
